e23a95924112cd2c516b666697d4ed29cedcbf72
Darsh Parikh <iamdarsh2424@gmail.com>
2025-02-21 22:32:22 +0530

    Fixed base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./context/ThemeContext";
 import { LoveProvider } from "./context/LoveContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/promise/">
     <ThemeProvider>
       <LoveProvider>
         <App />
